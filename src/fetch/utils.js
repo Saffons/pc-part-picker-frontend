60d1245a708903f8d3e77e01d39ae0533ba8cdcf
Mariusz Marszałek <ironsaffaz@gmail.com>
2024-01-04 21:06:43 +0100

    gke

--- src/fetch/utils.js
+++ src/fetch/utils.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://zti-mariusz.ew.r.appspot.com/";
-export const API_URL = "https://zti-mariusz.ew.r.appspot.com/api/";
-export const API_PARTS_URL = "https://zti-mariusz.ew.r.appspot.com/api/parts/";
+export const BASE_URL = "http://35.239.94.86:8080/";
+export const API_URL = "http://35.239.94.86:8080/api/";
+export const API_PARTS_URL = "http://35.239.94.86:8080/api/parts/";
