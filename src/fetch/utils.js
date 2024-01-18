0f9feca6863c4672bdf4b6a5b798b2a4d31a5f76
Mariusz Marszałek <ironsaffaz@gmail.com>
2024-01-19 00:38:16 +0100

    Add Dockerfile and change base API url to match my service url

--- src/fetch/utils.js
+++ src/fetch/utils.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://35.239.94.86:8080/";
-export const API_URL = "http://35.239.94.86:8080/api/";
-export const API_PARTS_URL = "http://35.239.94.86:8080/api/parts/";
+export const BASE_URL = "http://34.118.79.101:8080/";
+export const API_URL = "http://34.118.79.101:8080/api/";
+export const API_PARTS_URL = "http://34.118.79.101:8080/api/parts/";
