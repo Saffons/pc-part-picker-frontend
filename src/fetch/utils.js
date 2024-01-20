4cc49880e9553a5540f1aa61afb657fa4b1bed17
Mariusz Marszałek <ironsaffaz@gmail.com>
2024-01-20 21:32:47 +0100

    SSSSSSSSSSSSSSSSS

--- src/fetch/utils.js
+++ src/fetch/utils.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://asa-qo5x7ny3q7hme-standard-projekt-backend.azuremicroservices.io/";
-export const API_URL = "http://asa-qo5x7ny3q7hme-standard-projekt-backend.azuremicroservices.io/api/";
-export const API_PARTS_URL = "http://asa-qo5x7ny3q7hme-standard-projekt-backend.azuremicroservices.io/api/parts/";
+export const BASE_URL = "https://asa-qo5x7ny3q7hme-standard-projekt-backend.azuremicroservices.io/";
+export const API_URL = "https://asa-qo5x7ny3q7hme-standard-projekt-backend.azuremicroservices.io/api/";
+export const API_PARTS_URL = "https://asa-qo5x7ny3q7hme-standard-projekt-backend.azuremicroservices.io/api/parts/";
