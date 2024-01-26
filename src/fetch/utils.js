b28ac86dd8a24fb60a9bc45ee6f088190b89ef06
Mariusz Marszałek <ironsaffaz@gmail.com>
2024-01-26 03:42:53 +0100

    url change

--- src/fetch/utils.js
+++ src/fetch/utils.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://34.118.79.101:8080/";
-export const API_URL = "http://34.118.79.101:8080/api/";
-export const API_PARTS_URL = "http://34.118.79.101:8080/api/parts/";
+export const BASE_URL = "http://34.118.33.113:8080/";
+export const API_URL = "http://34.118.33.113:8080/api/";
+export const API_PARTS_URL = "http://34.118.33.113:8080/api/parts/";
